9cab9358daae76b993778623dd197a974124fd6c
Yurii Bilozerov <yuriibilozerov@gmail.com>
2023-01-24 21:25:44 +0100

    Add project template

---
+++ AppleSegmentationExample/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AppleSegmentationExample
+//
+//  Created by Yurii Bilozerov on 24.01.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ AppleSegmentationExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppleSegmentationExample
+//
+//  Created by Yurii Bilozerov on 24.01.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
