44c507283ecf9dfb5307f010680d5ff9cadda67d
Yurii Bilozerov <yuriibilozerov@gmail.com>
2023-01-31 14:44:35 +0100

    Implement camera frames displaying on application window
    - Implement `OutputView` based on `MetalKit`
    - Implement Metal setup and errors handling
    - Rename `ViewController` to `OutputViewController`

---
+++ AppleSegmentationExample/Interface/OutputView.swift
@@ -0,0 +1,117 @@
+//
+//  OutputView.swift
+//  AppleSegmentationExample
+//
+//  Created by Yurii Bilozerov on 29.01.2023.
+//  Copyright © 2023 Yurii Bilozerov. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+import MetalKit
+
+class OutputView: NSView {
+    
+    // MARK: - Properties
+    
+    var image: CIImage? {
+        didSet {
+            metalView.draw()
+        }
+    }
+    
+    private let metalView = MTKView()
+    private let metalDevice = MTLCreateSystemDefaultDevice()
+    private lazy var metalCommandQueue = metalDevice?.makeCommandQueue()
+    
+    private lazy var ciContext: CIContext? = {
+        guard let metalDevice else { return nil }
+        return CIContext(mtlDevice: metalDevice)
+    }()
+    
+    private let colorSpace = CGColorSpaceCreateDeviceRGB()
+    
+    // MARK: - Setup
+    
+    func setup() throws {
+        // Check for required properties
+        guard let metalDevice else { throw MetalSetupError.missingDevice }
+        guard metalCommandQueue != nil else { throw MetalSetupError.missingCommandQueue }
+        
+        // Setup Metal view
+        metalView.device = metalDevice
+        metalView.isPaused = true
+        metalView.delegate = self
+        metalView.framebufferOnly = false
+        
+        // Add Metal view and tie it to the bounds
+        addSubview(metalView)
+        metalView.translatesAutoresizingMaskIntoConstraints = false
+        metalView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        metalView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        metalView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        metalView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+    }
+}
+
+// MARK: - MTKViewDelegate
+
+extension OutputView: MTKViewDelegate {
+    
+    func draw(in view: MTKView) {
+        // Obtain image to draw
+        guard let image else { return }
+        
+        // Unwrap mandatory properties
+        guard let metalCommandQueue, let ciContext else { return }
+        
+        // Make command buffer to encode GPU instructions
+        guard let commandBuffer = metalCommandQueue.makeCommandBuffer() else { return }
+        
+        // Ensure drawable is free and not tied in the preivous drawing cycle
+        guard let currentDrawable = view.currentDrawable else { return }
+        
+        // TODO: Implement different content modes
+        // Scale image to fill Metal view
+        let drawSize = metalView.drawableSize
+        let scaleX = drawSize.width / image.extent.width
+        let scaleY = drawSize.height / image.extent.height
+        
+        let scaledImage = image.transformed(by: .init(scaleX: scaleX, y: scaleY))
+        
+        // Render image to the drawable
+        ciContext.render(
+            scaledImage,
+            to: currentDrawable.texture,
+            commandBuffer: commandBuffer,
+            bounds: scaledImage.extent,
+            colorSpace: colorSpace
+        )
+        
+        // Present drawable
+        commandBuffer.present(currentDrawable)
+        commandBuffer.commit()
+    }
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        // This delegate method is required
+    }
+}
+
+// MARK: - Errors
+
+extension OutputView {
+    
+    enum MetalSetupError: LocalizedError {
+        case missingDevice
+        case missingCommandQueue
+        
+        var errorDescription: String? {
+            return "Unable to setup Metal environment"
+        }
+        
+        var recoverySuggestion: String? {
+            return "The app can't work without Metal and will be terminated."
+        }
+    }
+}
---
+++ AppleSegmentationExample/Interface/OutputViewController.swift
@@ -0,0 +1,96 @@
+//
+//  OutputViewController.swift
+//  AppleSegmentationExample
+//
+//  Created by Yurii Bilozerov on 24.01.2023.
+//  Copyright © 2023 Yurii Bilozerov. All rights reserved.
+//
+
+import Cocoa
+import OSLog
+
+class OutputViewController: NSViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet private weak var outputView: OutputView!
+    
+    private var framesProvider = CameraFramesProvider()
+    private var isFramesProviderPrepared: Bool = false
+    
+    private var metalSetupError: OutputView.MetalSetupError?
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            // Try setup output view
+            try outputView.setup()
+        }
+        catch {
+            // Preserve setup error because app isn't able to show alert right here
+            metalSetupError = error as? OutputView.MetalSetupError
+        }
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        // Terminate the app if Metal isn't available
+        if let metalSetupError {
+            showAlert(with: metalSetupError)
+            NSApp.terminate(nil)
+        }
+        
+        // Prepare or resume obtaining frames from the camera
+        prepareFramesProviderIfNeeded()
+    }
+    
+    override func viewDidDisappear() {
+        super.viewDidDisappear()
+        
+        // Pause obtaining frames from the camera when window is minimized
+        framesProvider.delegate = nil
+    }
+}
+
+// MARK: - CameraFramesProviderDelegate
+
+extension OutputViewController: CameraFramesProviderDelegate {
+    
+    func framesProvider(_ provider: CameraFramesProvider, didOutput imageBuffer: CVImageBuffer) {
+        outputView.image = CIImage(cvImageBuffer: imageBuffer)
+    }
+}
+
+// MARK: - Private
+
+private extension OutputViewController {
+    
+    func prepareFramesProviderIfNeeded() {
+        // Just resume frames provider
+        if isFramesProviderPrepared {
+            framesProvider.delegate = self
+            return
+        }
+        
+        // Prepare frames provider
+        framesProvider.prepare { error in
+            if let error {
+                self.showAlert(with: error)
+                return
+            }
+            
+            self.framesProvider.delegate = self
+        }
+        
+        isFramesProviderPrepared = true
+    }
+    
+    func showAlert(with error: Error) {
+        let alert = NSAlert(error: error)
+        alert.runModal()
+    }
+}
